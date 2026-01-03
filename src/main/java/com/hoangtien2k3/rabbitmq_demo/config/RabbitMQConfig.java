97fd23f74d017b40fa78ad8fa5e7cd7443744df2
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2026-01-04 03:21:32 +0700

    Refactor RabbitMQ configuration by removing unused properties and simplifying the setup

--- src/main/java/com/hoangtien2k3/rabbitmq_demo/config/RabbitMQConfig.java
+++ src/main/java/com/hoangtien2k3/rabbitmq_demo/config/RabbitMQConfig.java
@@ -1,6 +1,5 @@
 package com.hoangtien2k3.rabbitmq_demo.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -8,14 +7,8 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Configuration
-@RequiredArgsConstructor
 public class RabbitMQConfig {
-
-    private final RabbitQueueProperties rabbitQueueProperties;
 
     @Bean
     public MessageConverter jsonMessageConverter() {
@@ -27,17 +20,5 @@ public class RabbitMQConfig {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(jsonMessageConverter());
         return template;
-    }
-
-    /**
-     * Expose the list of queues for QueueInitialization by extracting it from the
-     * properties bean.
-     */
-    @Bean
-    public List<RabbitQueueProperties> rabbitQueuePropertiesList() {
-        if (rabbitQueueProperties.getQueues() == null) {
-            return new ArrayList<>();
-        }
-        return new ArrayList<>(rabbitQueueProperties.getQueues().values());
     }
 }
